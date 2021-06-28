57855c010c1ab0c38ef56e14c2a4ea16b2e4e38c
Dusan <dusan.chmelko@yahoo.com>
2021-06-28 14:18:21 +0200

    api fix

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const KEY = process.env.REACT_APP_KEY_1;
-// const KEY = process.env.REACT_APP_KEY_2;
-const KEY = process.env.REACT_APP_KEY_3;
+const KEY = process.env.REACT_APP_KEY_2;
+// const KEY = process.env.REACT_APP_KEY_3;
 
 const youtube = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
